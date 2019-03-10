4a704b216102f25b8548ccbd6505aa40613c1136
Fareed Javed <fareedjaved@gmail.com>
2019-03-09 23:57:58 -0600

    Multiplication is now a domain object

--- social-multiplication/src/main/java/microservices/book/multiplication/domain/Multiplication.java
+++ social-multiplication/src/main/java/microservices/book/multiplication/domain/Multiplication.java
@@ -1,43 +1,24 @@
 package microservices.book.multiplication.domain;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+
 /**
  * 
- * This class represents a Multiplication in our 
- * application 
+ * This represents a Multiplication (a * b). 
  *
  */
-public class Multiplication {
-	private int factorA;
-	private int factorB;
+@RequiredArgsConstructor
+@Getter
+@ToString
+@EqualsAndHashCode
+public final class Multiplication {
+	private final int factorA;
+	private final int factorB;
 	
-	// The result of the operation (A * B)
-	private int result; 
-	
-	public Multiplication(int factorA, int factorB) {
-		this.factorA = factorA;
-		this.factorB = factorB;
-		this.result = factorA * factorB;
+	Multiplication() {
+		this(0, 0);
 	}
-	
-	public int getFactorA() {
-		return factorA;
-	}
-	
-	public int getFactorB() {
-		return factorB;
-	}
-	
-	public int getResult() {
-		return result;
-	}
-	
-	@Override
-	public String toString() {
-		return "Multiplication{" + 
-				"factorA=" + factorA +
-				", factorB=" + factorB + 
-				", result(A*B)=" + result +
-				+ '}';
-	}
-
 }
