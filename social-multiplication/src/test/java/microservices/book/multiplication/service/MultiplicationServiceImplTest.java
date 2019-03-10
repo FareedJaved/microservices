be7088d6fd2cf32d78dad44b9a31c58cc0153f3f
Fareed Javed <fareedjaved@gmail.com>
2019-03-09 23:56:02 -0600

    Refactored and added test for checkAttempt method

--- social-multiplication/src/test/java/microservices/book/multiplication/service/MultiplicationServiceImplTest.java
+++ social-multiplication/src/test/java/microservices/book/multiplication/service/MultiplicationServiceImplTest.java
@@ -6,6 +6,8 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import microservices.book.multiplication.domain.Multiplication;
+import microservices.book.multiplication.domain.MultiplicationResultAttempt;
+import microservices.book.multiplication.domain.User;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.BDDMockito.given;
@@ -40,9 +42,38 @@ public class MultiplicationServiceImplTest {
 		
 		//assert 
 		assertThat(multiplication.getFactorA()).isEqualTo(50);
-		assertThat(multiplication.getFactorB()).isEqualTo(30);
-		assertThat(multiplication.getResult()).isEqualTo(1500);
+		assertThat(multiplication.getFactorB()).isEqualTo(30);		
+	}
+	
+	@Test
+	public void checkCorrectAttemptTest() {
+		// given 
+		Multiplication multiplication = new Multiplication(50, 60);
+		User user = new User("john_doe");
+		MultiplicationResultAttempt attempt = 
+				new MultiplicationResultAttempt(user, multiplication, 3000);
 		
+		// when 
+		boolean attemptResult = 
+				multiplicationServiceImpl.checkAttempt(attempt);
+		
+		//assert
+		assertThat(attemptResult).isTrue();
+	}
+	
+	@Test
+	public void checkWrongAttemptTest() {
+		// given
+		Multiplication multiplication = new Multiplication(50, 60);
+		User user = new User("john_doe");
+		MultiplicationResultAttempt attempt = 
+				new MultiplicationResultAttempt(user, multiplication, 3010);
+		
+		// when
+		boolean attemptResult = multiplicationServiceImpl.checkAttempt(attempt);
+		
+		// then
+		assertThat(attemptResult).isFalse();
 	}
 
 }
